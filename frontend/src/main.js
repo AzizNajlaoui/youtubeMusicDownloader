f25118cc5ebe4533814773f2b8350b68a74d3385
aziz <medaziznajlaoui@gmail.com>
2026-01-28 23:30:40 +0100

    ..

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -12,7 +12,7 @@ let isLoading = false;
 function setLoading() {
   button.innerHTML = "";
   const img = document.createElement("img");
-  img.src = "./loader.svg";
+  img.src = "/loader.svg";
   img.className = "w-6 h-6 animate-spin";
   img.style.animationDuration = "1.3s";
   button.appendChild(img);
